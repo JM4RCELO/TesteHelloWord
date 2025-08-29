3b98079abca8d60c7217df88d7887647c4fed196
João Marques <joaomarcelojmrm98@gmail.com>
2025-08-29 18:04:52 +0100

    Novas mensagens dpara o utilizador

--- codigo_hw/codigo.c
+++ codigo_hw/codigo.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 
 void main() {
-    printf("Hello,   World!\n");
+    printf("Hello, World!\n");
     delay("1000");
+    printf("This is a C17 standard code.\n");
+    printf("Goodbye, World!\n");
+    //Vamos agora testar se deu o commit
     return 0;
 }
